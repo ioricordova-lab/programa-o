b9f843e18a30e2a5e11f560c09983bd2dd9c3e7f
ioricordova-lab <iori.cordova@escola.pr.gov.br>
2026-05-27 11:34:56 -0300

    Add files via upload

---
+++ programa-o-main/script.js
@@ -0,0 +1,16 @@
+const body = document.body;
+body.style.margin = '0';
+
+// ========== CABEÇALHO ==========
+const cabeçalho = document.createElement('h1');
+cabeçalho.textContent = 'criando elementos com JavaScript';
+
+cabeçalho.style.color = '#ffffff';
+cabeçalho.style.fontSize = '40px';
+cabeçalho.style.textAlign = 'center';
+cabeçalho.style.display = 'flex';
+cabeçalho.style.alignItems = 'center';
+cabeçalho.style.justifyContent = 'center';
+cabeçalho.style.backgroundColor = '#3a3281';
+cabeçalho.style.height = '100px';
+cabeçalho.style.margin = '0';
